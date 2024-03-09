c4c82955e875581c1443f547a477c68bbf7a20f4
matiunivar <mattff3@gmail.com>
2024-03-08 21:16:19 -0300

    fix react versio

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './styles.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './styles.css';
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const root = document.getElementById('app');
+ReactDOM.createRoot(root).render(<App />);
